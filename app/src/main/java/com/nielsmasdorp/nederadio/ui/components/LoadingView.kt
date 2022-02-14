5958492f48e64e47f357c45e8cbe18463cefe1f1
Niels Masdorp <nielsmasdorp@gmail.com>
2022-02-14 10:09:51 +0100

    Remove some docs

--- app/src/main/java/com/nielsmasdorp/nederadio/ui/components/LoadingView.kt
+++ app/src/main/java/com/nielsmasdorp/nederadio/ui/components/LoadingView.kt
@@ -24,7 +24,7 @@ import com.nielsmasdorp.nederadio.R
 fun LoadingView(
     modifier: Modifier = Modifier,
     backgroundColor: Color,
-    foregroundColor: Color
+    foregroundColor: Color // TODO fix animation color
 ) {
     Box(
         modifier = modifier
@@ -34,32 +34,12 @@ fun LoadingView(
     ) {
         val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.loading))
         val progress by animateLottieCompositionAsState(composition)
-        val dynamicProperties = rememberLottieDynamicProperties(
-            rememberLottieDynamicProperty(
-                property = LottieProperty.COLOR,
-                value = Color.Red.toArgb(),
-                keyPath = arrayOf(
-                    "播放动画",
-                    "路径 1",
-                    "形状图层 20",
-                    "填充 1",
-                    "变换",
-                    "矩形 1",
-                    "形状图层 19",
-                    "形状图层 18",
-                    "填充 1",
-                    "变换",
-                    "矩形 1"
-                )
-            )
-        ) // TODO fix color
         LottieAnimation(
             modifier = Modifier
                 .size(192.dp)
                 .align(Alignment.Center),
             composition = composition,
             progress = progress,
-            dynamicProperties = dynamicProperties
         )
     }
 }
