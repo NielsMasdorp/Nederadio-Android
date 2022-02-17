8dc6eabe9f9a69576a0916e87ba79c318e448dfc
Niels Masdorp <nielsmasdorp@gmail.com>
2022-02-17 19:29:27 +0100

    update about dialog

--- app/src/main/java/com/nielsmasdorp/nederadio/ui/components/dialog/AboutAppDialog.kt
+++ app/src/main/java/com/nielsmasdorp/nederadio/ui/components/dialog/AboutAppDialog.kt
@@ -13,6 +13,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.nielsmasdorp.nederadio.BuildConfig
 import com.nielsmasdorp.nederadio.R
 import com.nielsmasdorp.nederadio.ui.components.HtmlText
 
@@ -37,7 +38,7 @@ fun AboutAppDialog(
             )
         },
         text = {
-            HtmlText(html = stringResource(id = R.string.about_message))
+            HtmlText(html = stringResource(id = R.string.about_message, BuildConfig.VERSION_NAME))
         },
         confirmButton = {
             Row(
